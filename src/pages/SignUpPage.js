1100401b026540ac2ef285f4e5748fb5d052a2f6
Chan Nyein Tun <chanyeintun@gmail.com>
2022-12-10 23:39:31 +0630

    feat:unit testing for interactions

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,16 +1,40 @@
+import { useState } from "react";
+
 const SignUpPage = () => {
+    const [password, setPassword] = useState();
+    const [passwordRepeat, setPasswordRepeat] = useState();
+
+    const onChangePassword = (e) => {
+        setPassword(e.target.value);
+    };
+    const onChangeRepeatPassword = (e) => {
+        setPasswordRepeat(e.target.value);
+    };
+
+    let disabled = true;
+
+    if (password && passwordRepeat) {
+        disabled = password !== passwordRepeat;
+    }
+    
     return (
         <div>
             <h1>Sign Up</h1>
             <label htmlFor="username">Username</label>
-            <input id="username" type="text"/>
+            <input id="username" type="text" />
             <label htmlFor="email">E-mail</label>
-            <input id="email" type="email"/>
+            <input id="email" type="email" />
             <label htmlFor="password">Password</label>
-            <input id="password" type="password"/>
+            <input id="password" type="password" onChange={onChangePassword} />
             <label htmlFor="passwordRepeat">Password Repeat</label>
-            <input id="passwordRepeat" type="password"/>
-            <button disabled>Sign Up</button>
+            <input
+                id="passwordRepeat"
+                type="password"
+                onChange={onChangeRepeatPassword}
+            />
+            <button type="button" disabled={disabled}>
+                Sign Up
+            </button>
         </div>
     );
 };
